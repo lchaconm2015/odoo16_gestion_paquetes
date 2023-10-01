40df1a52ae5767e38697150e717671d717db2698
lester <lchaconm2015@gmail.com>
2023-10-01 00:32:28 -0400

    [commit] First commit , sistema de gestion de paquetes

--- gestion_paquetes/controllers/controllers.py
+++ gestion_paquetes/controllers/controllers.py
@@ -32,20 +32,22 @@ class CustomPortal(http.Controller):
             if obj_stock_quant:
                 mensaje = 'Su paquete fue encontrado en nuestro sistema'
                 show_detail = True
+                partner = None
+                shipping_addres = ''
+                object_stock_picking = request.env['stock.picking'].search(
+                    [('product_id.name', '=', post['package_number'])])
+                obj_account_move = request.env['account.move'].search(
+                    [('invoice_picking_id.id', '=', object_stock_picking.id)])
+
+                partner = obj_account_move.partner_id.name
+
+                package_number = obj_stock_quant.product_id.name
+                shipping_addres = obj_account_move.partner_shipping_id.street
+                location = obj_stock_quant.location_id.name
+                invoice_number = obj_account_move.name
             else:
                 mensaje = 'Su paquete no fue encontrado en nuestros almacenes, rectifique su número de paquete.'
-            partner = None
-            shipping_addres = ''
-            object_stock_picking = request.env['stock.picking'].search([('product_id.name', '=', post['package_number'])])
-            obj_account_move = request.env['account.move'].search(
-                [('invoice_picking_id.id', '=', object_stock_picking.id)])
 
-            partner = obj_account_move.partner_id.name
-
-            package_number = obj_stock_quant.product_id.name
-            shipping_addres = obj_account_move.partner_shipping_id.street
-            location = obj_stock_quant.location_id.name
-            invoice_number = obj_account_move.name
 
         return request.render('gestion_paquetes.search_package_id',
                               {'mensaje': mensaje, 'package_number': package_number,
