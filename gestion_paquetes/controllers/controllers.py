288ebf47603f91da7050114045c705d70d564304
lester <lchaconm2015@gmail.com>
2023-09-30 22:43:30 -0400

    [commit] First commit , sistema de gestion de paquetes

--- gestion_paquetes/controllers/controllers.py
+++ gestion_paquetes/controllers/controllers.py
@@ -22,15 +22,32 @@ class CustomPortal(http.Controller):
     @route(['/search_package/submit'], type='http', auth='user', website=True)
     def search_package_submit(self, redirect=None, **post):
         mensaje = ''
+        show_detail = False
+        obj_stock_quant = None
         if 'package_number' in post:
             obj_stock_quant = request.env['stock.quant'].sudo().search(
                 [('product_id.name', '=', post['package_number']),
                  ('company_id', '=', request.env['website'].get_current_website().company_id.id),
                  ('inventory_quantity_auto_apply', '=', 1)])
             if obj_stock_quant:
-                mensaje = 'Su paquete se encuentra en nuestra ubicacion de:{}'.format(obj_stock_quant.location_id.name)
+                mensaje = 'Su paquete fue encontrado en nuestro sistema'
+                show_detail = True
             else:
                 mensaje = 'Su paquete no fue encontrado en nuestros almacenes, rectifique su número de paquete.'
+            partner = None
+            shipping_addres = ''
+            object_stock_move = request.env['stock.move'].search([('product_id.name', '=', post['package_number'])])
+            obj_account_move = request.env['account.move'].search(
+                [('invoice_picking_id.id', '=', object_stock_move.id)])
+
+            partner = obj_account_move.partner_id.name
+
+            package_number = obj_stock_quant.product_id.name
+            shipping_addres = obj_account_move.partner_shipping_id.street
+            location = obj_stock_quant.location_id.name
+            invoice_number = obj_account_move.name
 
         return request.render('gestion_paquetes.search_package_id',
-                              {'mensaje': mensaje})
+                              {'mensaje': mensaje, 'package_number': package_number,
+                               'shipping_addres': shipping_addres, 'invoice_number': invoice_number,
+                               'partner_name': partner, 'location': location, 'show_detail': show_detail})
